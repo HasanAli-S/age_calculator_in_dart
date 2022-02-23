1b6ed5f2d528c2825ffdc18fd02a83c1e3b1172d
HasanAli-S <77610547+HasanAli-S@users.noreply.github.com>
2022-02-23 10:28:12 -0800

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,16 @@
+main(){
+  int age=20;
+  int days=age*365;
+  print('Days=$days');
+  
+  int weeks=days~/7;
+  print('Weeks=$weeks');
+  
+  int hours=days*24;
+  print('Hours=$hours');
+
+  int sec=hours*3600;
+  print('Seconds=$sec');
+  
+
+}
